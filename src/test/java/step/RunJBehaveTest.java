2fd7abe333b040b9518afff060c22804bc14626e
Mykola Gladchenko <mykola.gladchenko@gmail.com>
2020-12-18 02:33:12 +0200

    updated metafilter in RunJBehaveTest

--- src/test/java/step/RunJBehaveTest.java
+++ src/test/java/step/RunJBehaveTest.java
@@ -3,6 +3,6 @@ package step;
 import net.serenitybdd.jbehave.SerenityStories;
 import net.serenitybdd.jbehave.annotations.Metafilter;
 
-@Metafilter("+disb-validations")
+@Metafilter("+disb")
 public class RunJBehaveTest extends SerenityStories {
 }
